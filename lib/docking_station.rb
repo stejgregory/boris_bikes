d0c0ff07ff756aa77be2cb11e441dd7b8b9035ee
Tom Nunn <t@tomnunn.uk>
2016-09-27 17:04:07 +0100

    step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
-require_relative 'bike'
+# require_relative 'bike'
 
 class DockingStation
+
  def release_bike
    Bike.new
  end
+
+ def dock(bike)
+   # We need to return the bike we dock
+   bike
+ end
+
+ def bike
+ end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,21 +1,15 @@
 require 'docking_station'
 
-#describe DockingStation do
-#  it 'responds to release bike' do
-#    expect(subject).to respond_to :release_bike
-#  end
-#
-#end
-
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
-end
-
-describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
-
+  it 'docks something' do
+    bike = Bike.new
+    # We want to return the bike we dock
+    expect(subject.dock(bike)).to eq bike
+  end
+  it { is_expected.to respond_to :release_bike }
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+  it { is_expected.to respond_to(:bike) }
 end
