ea299eff463e74013b0e558b62e092dfe9e8e8ba
michkles <michkles@gmail.com>
2016-09-27 08:13:20 +0100

    step8 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,4 @@
 class DockingStation
-  def release_bike
-  end
-end  
+ def release_bike
+ end
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require 'docking_station'
 
-describe DockingStation do
+#describe DockingStation do
+#  it 'responds to release bike' do
+#    expect(subject).to respond_to :release_bike
+#  end
+#
+#end
 
+describe DockingStation do
+  it {is_expected.to respond_to :release_bike}
 end
