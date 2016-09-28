bb63bc1589222a5b7326eb4dd3f7edc8f1784f88
Tom Nunn <t@tomnunn.uk>
2016-09-28 15:57:56 +0100

    add more to feature test for dock

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
-
   attr_reader :bike
 
- def release_bike
-   fail 'No bikes available' unless @bike
-   @bike
- end
+  def initialize
+    @bikes = []
+  end
 
- def dock(bike)
-  fail 'Docking station full' if @bike 
-  @bike = bike
- end
+  def release_bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
+  end
 
+  def dock(bike)
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
+  end
 end
