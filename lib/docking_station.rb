51c6e8e0269d314b03fe7108a2db12fc2e266939
Tom Nunn <t@tomnunn.uk>
2016-09-29 11:52:22 +0100

    add constant in DockingStation Class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,21 +1,31 @@
 require_relative 'bike'
 
 class DockingStation
+  DEFAULT_CAPACITY = 20
 
   attr_reader :bike
 
   def initialize
     @bikes = []
-    @capacity = 20
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= @capacity
+    fail 'Docking station full' if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
