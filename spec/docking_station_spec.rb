91947b2523047072f9d74a3b25498f636831323e
Stephen Gregory <stejgregory@gmail.com>
2016-09-29 17:17:54 +0100

    Refactored Test to include DEFAULT CAPACITY

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -31,10 +31,14 @@ describe DockingStation do
   # end
 
   describe 'initialization' do
-    it 'has a variable capacity' do
-      docking_station = DockingStation.new(50)
-      50.times { docking_station.dock Bike.new }
-      expect { docking_station.dock Bike.new }.to raise_error 'Docking station full'
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+      expect { subject.dock(bike) }.to raise_error 'Docking station full'
     end
   end
+  
 end
